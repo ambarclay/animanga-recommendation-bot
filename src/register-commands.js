fee00e12520970a136a18ffd3b180aa8ea0945db
Angelena Barclay <angelenabarclay@gmail.com>
2025-03-21 10:04:37 -0400

    removed guild id requirement

--- src/register-commands.js
+++ src/register-commands.js
@@ -34,7 +34,7 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
   try {
     console.log("Registering slash commands...");
 
-    await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), {
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
       body: commands,
     });
 
